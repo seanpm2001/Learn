5c66dff4451987317407a7efad4f200586bf7d65
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2023-12-21 15:08:45 -0800

    NEWLS_2023.12.26.cob
    NEWLS_2023.12.26.cob

---
+++ Docs/NewLS/2023/NEWLS_2023.12.26.cob
@@ -0,0 +1,3 @@
+write "NEWLS_2023.12.26.cob"
+write "I can't find any usable data for modern programming with FLOW-MATIC, so I am guessing on everything."
+write "It resembles COBOL, is the predecessor to COBOL, but isn't entirely the same"
