aa4892747732cfa8d0a3904bf1f482e02dc15e81
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2023-12-21 15:00:21 -0800

    NEWLS_2023.12.25_CPL.c

---
+++ Docs/NewLS/2023/NEWLS_2023.12.25_CPL.c
@@ -0,0 +1,4 @@
+Write("NEWLS_2023.12.25_CPL.c")
+Write("String")
+Write("I have looked and looked, going through 2 PDF documents and a few webpages to try and find what remains of this language, and cannot find anything about comments or the file extension used. I luckily was able to find string output, which I will use instead. For the file extension, I will guess")
+Write("File version: 1 (2023, Thursday, December 21st at 3:00 pm PST")
