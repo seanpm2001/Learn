b0cb926e7f558367f28ff1777bb6a26b8b75ae4d
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2023-12-21 15:23:02 -0800

    NEWLS_2023.12.29_asm.js
    NEWLS_2023.12.29_asm.js

---
+++ Docs/NewLS/2023/NEWLS_2023.12.29_asm.js
@@ -0,0 +1,10 @@
+// NEWLS_2023.12.29_asm.js
+// asm.js
+// Same as JavaScript... mostly
+console.log("String");
+/* Multi-line
+comment */
+/* asm.js was an experimental subset of
+JavaScript by Mozilla that was deprecated
+and superceded by WebAssembly in 2017 */
+// File version: 1 (2023, Thursday, December 21st at 3:23 pm PST)
