0a6923d5c05fc978f6ccc58c42098b6a5fd2f6e6
Sean P. Myrick V19.1.7.2 <seanpm2001-mailing-list@protonmail.com>
2023-12-23 18:55:05 -0800

    NEWLS_2024.01.02_POP-1_COWSEL.c
    NEWLS_2024.01.02_POP-1_COWSEL.c

---
+++ Docs/NewLS/2024/NEWLS_2024.01.02_POP-1_COWSEL.c
@@ -0,0 +1,9 @@
+function fun1
+lambda x y
+comment This is a comment
+define x * y
+comment These four reserved keywords were sourced from Wikipedia: https://en.wikipedia.org/w/index.php?title=COWSEL&oldid=1176003718&useskin=monobook
+comment POP-1 was originally named COWSEL (which stands for: COntrolled Working SpacE Language)
+comment POP-1 was inspired by CPL and Lisp
+comment I don't know the extension POP-1 uses, so I am guessing the letter C as the file extension 
+comment POP-1 was developed between 1964 and 1966
